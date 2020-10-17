e56e4890b68d82215abcff62e0f02ecb4e107e30
i_am_akash_ <ay157311@gmail.com>
2020-10-17 18:44:52 +0530

    view-engine

--- app.js
+++ app.js
@@ -3,27 +3,33 @@ const express= require('express');
 // Creating an express app
 const app= express();
 
+// set view engine
+app.set('view engine','ejs');   // Express will automatically check in views folder for file.
+// if we want to put another name instead of views than we have use below code to specify out folder name
+// app.set('views','myViewName');
 
 // Routing
-app.get('/',(req,res)=>{
-    // res.send('Hello Ninjas');
-    res.sendFile('./views/index.html', {root:__dirname});
-});
-
-app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html', {root:__dirname});
-});
-
-// Redirects
-app.get('/about-me',(req,res)=>{
-    res.redirect('/about');
-});
-
-// 404 Error 
-// this is middelware it will run or use for every incoming request when the code will reach to this point 
-app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html', {root:__dirname});
-});
+app.get('/', (req, res) => {
+    const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs });
+  });
+  
+  app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+  });
+  
+  app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
+  
+  // 404 page
+  app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+  });
 
 // Listen to port
 app.listen(3000,(err)=>{
